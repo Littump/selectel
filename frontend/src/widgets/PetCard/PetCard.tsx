d3afe821061e8d20edbc75b7565fa2feb27d5310
p0ntific <ddsjs0626@gmail.com>
2024-02-24 12:04:27 +0300

    footer

--- frontend/src/widgets/PetCard/PetCard.tsx
+++ frontend/src/widgets/PetCard/PetCard.tsx
@@ -1,9 +1,38 @@
-// import styles from './PetCard.module.scss'
+import styles from './PetCard.module.scss'
+import { Text } from 'shared'
+interface IPetData{
+	id?: number
+	author?: any
+	pet?: any,
+	type?: string
+	breed?: string
+	need_blood_types?: string
+	contact?: string
+	blooud_amout?: number
+	address?: string
+	city?: string
+	data?: string
+}
+const PetCard = (props: IPetData) => {
 
-
-const PetCard = () => {
 	return (
-		<div>
+		<div className={styles.card}>
+			{/* <Text type={'medium'} fontSize={30} className={styles.card__title}>{props.type}</Text>
+			<Text type={'medium'} fontSize={30} className={styles.card__title}>{props.need_blood_types?.split(';')}</Text>
+			<Text type={'medium'} fontSize={20}>Владелец: {`${props.author.first_name} ${props.contact}`}</Text>
+			<Text type={'medium'} fontSize={20}>Дата: ${props.date}</Text>
+			<Text type={'medium'} fontSize={20}>Порода: {props.pet.breed}</Text>
+			<Text type={'medium'} fontSize={20}>Нооходимое количество крови: `${props.blooud_amout} мг`</Text>
+			<Text type={'medium'} fontSize={20}>Адрес: {props.address}, {props.city} </Text>
+			<img src={props.pet.avatar} /> */}
+			<Text type={'medium'} fontSize={30} className={styles.card__title}>{props.type}</Text>
+			<Text type={'medium'} fontSize={30} className={styles.card__title}>{props.type}</Text>
+			<Text type={'medium'} fontSize={20}>Владелец: {`${props.author.first_name} ${props.contact}`}</Text>
+			<Text type={'medium'} fontSize={20}>{props.need_blood_types?.split(';')}</Text>
+			<Text type={'medium'} fontSize={20}>Порода: {props.pet.breed}</Text>
+			<Text type={'medium'} fontSize={20}>Нооходимое количество крови: `${props.blooud_amout} мг`</Text>
+			<Text type={'medium'} fontSize={20}>Адрес: {props.address}, {props.city} </Text>
+			<img src={props.pet.avatar} />
 
 		</div>
 	)
--- frontend/src/widgets/model/RegistrationSlice.tsx
+++ frontend/src/widgets/model/RegistrationSlice.tsx
@@ -1,8 +1,11 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { _apiUrl } from "shared";
 interface ILogindata {
-    username: string;
-    password: string;
+	username: string
+	password: string
+	allPets: null
+	myPets: null
+	allAdvs: null
 }
 
 export const fetchLogin = createAsyncThunk(
@@ -30,27 +33,107 @@ export const fetchLogin = createAsyncThunk(
 );
 
 export const fetchRegister = createAsyncThunk(
-    "fetchRegister",
-    async (body: ILogindata) => {
-        try {
-            const response = await fetch(`${_apiUrl}/api/users/`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json;charset=utf-8",
-                },
-                body: JSON.stringify(body),
-            });
-            if (response.ok) {
-                const data = await response.json();
-                return data.results;
-            } else {
-                return response.status;
-            }
-        } catch (error) {
-            return error;
-        }
-    }
-);
+	'fetchRegister',
+	async (body : ILogindata) => {
+		try {
+			const response = await fetch(
+			  `/api/users/`,
+			  {
+				method: 'POST',
+				headers: {
+				  'Content-Type': 'application/json;charset=utf-8',
+				},
+				body: JSON.stringify(body)
+			  }
+			)
+			if (response.ok) {
+			  const data = await response.json()
+			  return data.results
+			} else {
+			  return response.status
+			}
+		  } catch (error) {
+			return error
+		  }
+		}
+)
+export const fetchPets = createAsyncThunk(
+	'fetchPets',
+	async () => {
+		try {
+			const response = await fetch(
+			  `${_apiUrl}/api/pets/`,
+				{
+					method: 'GET',
+					headers: {
+						'Content-Type': 'application/json;charset=utf-8',
+						Authorization: `Token ${localStorage.getItem('token')}`,
+					},
+				}
+			)
+			if (response.ok) {
+			  const data = await response.json()
+			  return data.results
+			} else {
+			  return response.status
+			}
+		  } catch (error) {
+			return error
+		  }
+		}
+)
+export const fetchMyPets = createAsyncThunk(
+	'fetchMyPets',
+	async () => {
+		try {
+			const response = await fetch(
+			  `${_apiUrl}/api/pets/me/`,
+				{
+					method: 'GET',
+					headers: {
+						'Content-Type': 'application/json;charset=utf-8',
+						Authorization: `Token ${localStorage.getItem('token')}`,
+					},
+				}
+			)
+			if (response.ok) {
+			  const data = await response.json()
+			  return data.results
+			} else {
+			  return response.status
+			}
+		  } catch (error) {
+			return error
+		  }
+	}
+)
+
+export const fetchAllAdvertisements = createAsyncThunk(
+	'fetchAllAdvertisements',
+	async (body) => {
+		try {
+			const response = await fetch(
+			  `${_apiUrl}/api/advertisements/`,
+				{
+					method: 'GET',
+					headers: {
+						'Content-Type': 'application/json;charset=utf-8',
+						Authorization: `Token ${localStorage.getItem('token')}`,
+					},
+					body: JSON.stringify(body)
+				}
+			)
+			if (response.ok) {
+			  const data = await response.json()
+			  return data.results
+			} else {
+			  return response.status
+			}
+		  } catch (error) {
+			return error
+		  }
+		}
+)
 
 const initialState: any = {
     loginStatus: "idle",
@@ -58,34 +141,80 @@ const initialState: any = {
 };
 
 const RegistrationSlice = createSlice({
-    name: "registration",
-    initialState,
-    reducers: {},
-    extraReducers: (builder) => {
-        builder
-            // fetchLogin
-            .addCase(fetchLogin.pending, (state) => {
-                state.loginStatus = "loading";
-            })
-            .addCase(fetchLogin.fulfilled, (state, action) => {
-                localStorage.setItem("token", action.payload);
-                state.loginStatus = "success";
-            })
-            .addCase(fetchLogin.rejected, (state) => {
-                state.loginStatus = "error";
-            })
-            // fetchRegister
-            .addCase(fetchRegister.pending, (state) => {
-                state.registerStatus = "loading";
-            })
-            .addCase(fetchRegister.fulfilled, (state, action) => {
-                state.registerStatus = "success";
-                localStorage.setItem("token", action.payload);
-            })
-            .addCase(fetchRegister.rejected, (state) => {
-                state.registerStatus = "error";
-            });
-    },
-});
+	name: 'registration',
+  	initialState,
+	reducers: {
+
+	},
+	extraReducers: (builder) => {
+		builder
+		// fetchLogin
+		.addCase(fetchLogin.pending, (state) => {
+			state.loginStatus = 'loading'
+		})
+		.addCase(fetchLogin.fulfilled, (state, action) => {
+			console.log(action.payload, typeof action.payload)
+			if (typeof action.payload !== 'number') {
+				localStorage.setItem('token', action.payload)
+				state.loginStatus = 'success'
+			} else {
+				state.loginStatus = 'error'
+			}
+		})
+		.addCase(fetchLogin.rejected, (state) => {
+			state.loginStatus = 'error'
+		})
+
+		// fetchRegister
+		.addCase(fetchRegister.pending, (state) => {
+			state.registerStatus = 'loading'
+		})
+		.addCase(fetchRegister.fulfilled, (state, action) => {
+			console.log(action.payload, typeof action.payload)
+			if (typeof action.payload !== 'number') {
+				state.registerStatus = 'success'
+				localStorage.setItem('token', action.payload)
+			} else {
+				state.registerStatus = 'error'
+			}
+		})
+		.addCase(fetchRegister.rejected, (state) => {
+			state.registerStatus = 'error'
+		})
+
+		// fetchPets
+		.addCase(fetchPets.pending, (state) => {
+			// state.loginStatus = 'loading'
+		})
+		.addCase(fetchPets.fulfilled, (state, action) => {
+			// state.loginStatus = 'success'
+			state.allPets = action.payload.results
+		})
+		.addCase(fetchPets.rejected, (state) => {
+		})
+
+		// fetchMyPets
+		.addCase(fetchMyPets.pending, (state) => {
+			// state.loginStatus = 'loading'
+		})
+		.addCase(fetchMyPets.fulfilled, (state, action) => {
+			// state.loginStatus = 'success'
+			state.myPets = action.payload.results
+		})
+		.addCase(fetchMyPets.rejected, (state) => {
+		})
+
+		// fetchAllAdvertisements
+		.addCase(fetchAllAdvertisements.pending, (state) => {
+			// state.loginStatus = 'loading'
+		})
+		.addCase(fetchAllAdvertisements.fulfilled, (state, action) => {
+			// state.loginStatus = 'success'
+			state.	allAdvs = action.payload.results
+		})
+		.addCase(fetchAllAdvertisements.rejected, (state) => {
+		})
+	}
+})
 
 export default RegistrationSlice.reducer;
